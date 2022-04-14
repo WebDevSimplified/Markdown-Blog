faa8224b3ebd5c6fdc80883f611e8081750afafc
TahiJameel <tahajameel.co@gmail.com>
2022-04-14 22:08:47 +0500

    Fix mongoose connection error

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const Article = require('./models/article')
-const articleRouter = require('./routes/articles')
-const methodOverride = require('method-override')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const Article = require('./models/article');
+const articleRouter = require('./routes/articles');
+const methodOverride = require('method-override');
+const app = express();
 
-mongoose.connect('mongodb://localhost/blog', {
-  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
-})
+mongoose.connect('mongodb://localhost/blog');
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-app.use(methodOverride('_method'))
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 
 app.get('/', async (req, res) => {
-  const articles = await Article.find().sort({ createdAt: 'desc' })
-  res.render('articles/index', { articles: articles })
-})
+  const articles = await Article.find().sort({ createdAt: 'desc' });
+  res.render('articles/index', { articles: articles });
+});
 
-app.use('/articles', articleRouter)
+app.use('/articles', articleRouter);
 
-app.listen(5000)
+app.listen(5000);
