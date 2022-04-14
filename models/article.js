0914461aeabb7b43453834102cad9ef35bba8cf5
TahiJameel <tahajameel.co@gmail.com>
2022-04-14 22:23:17 +0500

    Add Title on every single page

--- models/article.js
+++ models/article.js
@@ -8,31 +8,31 @@ const dompurify = createDomPurify(new JSDOM().window)
 const articleSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true
+    required: true,
   },
   description: {
-    type: String
+    type: String,
   },
   markdown: {
     type: String,
-    required: true
+    required: true,
   },
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   slug: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   sanitizedHtml: {
     type: String,
-    required: true
-  }
+    required: true,
+  },
 })
 
-articleSchema.pre('validate', function(next) {
+articleSchema.pre('validate', function (next) {
   if (this.title) {
     this.slug = slugify(this.title, { lower: true, strict: true })
   }
